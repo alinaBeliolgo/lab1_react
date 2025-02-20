7ea951d8c6968419e91f1022e6c293299dac57d5
alinaBeliolgo <159250463+alinaBeliolgo@users.noreply.github.com>
2025-02-20 21:26:52 +0200

    Add files via upload

---
+++ src/components/ArticleList.jsx
@@ -0,0 +1,14 @@
+import Article from "./Article"
+
+function ArticleList() {
+    return (
+      <>
+        <Article title="Первая статья" text="что-то написано" />
+        <Article title="Вторая" text="что-то написано" />
+        <Article title="Третья" text="что-то написано" />
+        <Article title="Четвертая" text="что-то написано" />
+      </>
+    );
+  }
+  
+  export default ArticleList;
